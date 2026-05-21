2edb8ab0bf52ffcfed05996566232a222f62d01f
Sardor <sardorputin@gmail.com>
2026-05-21 13:11:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -45,6 +45,8 @@ DATABASE_URL = os.getenv(
 
 REQUIRED_CHANNELS = {
     "NodeLink news": "@NodeLink_news",
+    "Bedrich": "@bedrich_tgk",
+    "Inviolable": "@inviolablebog"
 }
 
 # Referral mode:
